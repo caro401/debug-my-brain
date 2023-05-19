79d558d03ff59c58e1f3de4e150eacef635de087
Caro Appleby <caroline.appleby@cantab.net>
2023-05-19 18:13:08 +0100

    Actually read sveltekits docs on service workers

--- static/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-	e.waitUntil(caches.open('brain-store').then((cache) => cache.addAll(['/favicon.png'])));
-});
-
-self.addEventListener('fetch', (e) => {
-	console.log(e.request.url);
-	e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
-});
